44ce07add37fc6c4bc3f3d00154b9a765c5b9c55
RemediosM <puszka004@gmail.com>
2019-04-08 22:17:38 +0200

    Second Commit

--- src/main/java/com/kodilla/kalkulator/Kalkulator.java
+++ src/main/java/com/kodilla/kalkulator/Kalkulator.java
@@ -1,4 +1,29 @@
 package com.kodilla.kalkulator;
 
 public class Kalkulator {
+    private double a;
+    private double b;
+
+    public Kalkulator(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void dodaj() {
+        final double v = a + b;
+        System.out.println(a + " + " + b + " = " + v);
+    }
+
+    public void odejmij() {
+        final double v = a - b;
+        System.out.println(a + " - " + b + " = " + v);
+    }
+
+
+    public static void main(String args[]) {
+        Kalkulator kalkulator = new Kalkulator(2.7, 5);
+        kalkulator.dodaj();
+        kalkulator.odejmij();
+    }
 }
+
